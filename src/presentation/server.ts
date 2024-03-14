67906f50e5e84f700590e3db496a956815b076a2
Marcela Trujillo Toro <mt@fullstacksolvers.com>
2024-03-14 15:40:13 -0500

    feat: rest server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
   port: number;
+  router: Router;
   publicPath?: string;
 }
 
@@ -10,18 +11,34 @@ export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
-  constructor(options: Options) {
-    const { port, publicPath: public_path = 'public' } = options;
+  constructor({ port, publicPath = 'public', router }: Options) {
     this.port = port;
-    this.publicPath = public_path;
+    this.publicPath = publicPath;
+    this.routes = router;
   }
 
   async start() {
     //* Middlewares
+    //? funcion que se va a ejecutar cuando una peticion pase por ahi
+
+    //? parcear la informacion del body y volverla json
+    this.app.use(express.json());
+
+    //?habilitar x-www-form-urlencoded (muy usado en angular)
+    this.app.use(express.urlencoded({ extended: true }));
+
+    //-----------
 
     //* Public Folder
     this.app.use(express.static(this.publicPath));
+
+    //----------
+
+    //* routes
+
+    this.app.use(this.routes);
 
     //! cuando no se estaba haciendo esto, al recargar la pagina salia error cannot get ya con esto no se estalla la app al recargar
     this.app.get('*', (req, res) => {
